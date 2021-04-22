2d08b2d8d7acdfb3c08d184b547d8e8aedea06a0
Izzet85 <70436250+Izzet85@users.noreply.github.com>
2021-04-22 14:34:40 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-jm96hnjf.eu.auth0.com'
+      clientId='oKkodM459PyLlbbW2evgvYBH0bInaFYu'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 
-const PrivateRoute = () => {
-  return <h2>private route component</h2>;
-};
-export default PrivateRoute;
+const PrivateRoute = ({children,...rest}) => {
+  const isUser = true;
+  return <Route{...rest} render={()=>{return isUser?children}} >private route  </Route>
+}
+export default PrivateRoute
